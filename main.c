3bde42548ea3062d8f92ee2fc7fced7f048b98c1
Sailesh M <43719932+Saileshmurali@users.noreply.github.com>
2021-04-10 20:40:11 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,67 @@
+#include "stm32f407xx.h"
+void configureLED(void);
+void ADC_Config(void);
+void dma_config(void);
+uint8_t value[8000],value1[8000];
+uint32_t add1=(uint32_t)value,reg,length=8000;
+uint32_t add2=(uint32_t)value1;
+void ADC_Config(void)
+{
+	RCC->AHB1ENR |= (1UL<<0);// Enable clock for Port A
+	RCC->APB2ENR |= (1UL<<8);  // Enable ADC1 clock
+	ADC->CCR |= (1UL<<16);  //Pre-scale clock to ADC by 4
+	ADC1->CR1 |= (2UL<<24); //8 bit ADC
+	ADC1->CR2 |= (1UL<<1);  //Enable continuous conversion
+	ADC1->CR2 |= (1UL<<10); //Enable EOC after every conversion
+	ADC1->SMPR2 |= (7UL<<3);//Select sampling time as 480 cycles for channel 1
+	GPIOA->MODER |= (3UL<<2); //Configure PA1 in analog mode
+	ADC1->SQR3 |= (1<<0);//Select channel 1 in ADC (Corresponds to PA1)
+}
+void dma_config(void)
+{
+	RCC->AHB1ENR |=(1UL<<22);//Enable DMA2 clock
+	ADC1->CR2 |= (1UL<<8);  //Enable DMA
+	ADC1->CR2 |= (1UL<<9);
+	DMA2->LIFCR |= 0x7DUL;//Clear all interrupt flags
+	DMA2_Stream0->CR &=~(7UL<<25);//Select channel 0 of DMA
+	DMA2_Stream0->CR &=~(3UL<<13);//Set memory of DMA to 8bit per data
+	DMA2_Stream0->CR &=~(3UL<<11);//Peripheral size to 8 bit per data
+	DMA2_Stream0->CR |=(1UL<<10);//Memory increment mode
+	DMA2_Stream0->CR &=~(3UL<<6);//Data transfer from peripheral to memory
+	DMA2_Stream0->CR |=(3UL<<3);//Transfer full and half complete interrupt Enable
+	DMA2_Stream0->CR |= (1UL << 8);//Enable circular mode
+	DMA2_Stream0->CR |=(1UL<<18);//Enable Double buffer mode
+	DMA2_Stream0->CR &=~(1UL<<19);//Memory 0 is first target followed my Memory 1
+	DMA2_Stream0->NDTR=length;//Set DMA length to 8000
+	DMA2_Stream0->M0AR=add1;//Select destination memory 0 address as buffer declared
+	DMA2_Stream0->M1AR=add2;//Select destination memory 1 address as buffer declared
+	reg=(uint32_t)&ADC1->DR;
+	DMA2_Stream0->PAR=reg;//Select peripheral address as ADC1 data register
+	NVIC_EnableIRQ(DMA2_Stream0_IRQn);//Enable DMA Interrupt
+}
+void configureLED(void)
+{
+	RCC->AHB1ENR |=(1UL<<3);  //Enable GPIOD clock
+	GPIOD->MODER &= ~(0xFFUL<<12*2);
+	GPIOD->MODER |= (0x55UL<<12*2);//Set Pins PD12-15 as output
+}
+
+void DMA2_Stream0_IRQHandler(void)
+{
+	DMA2->LIFCR |= 0x7DUL;//Clear all interrupt flags
+	GPIOD->ODR ^=(1UL<<12);//Toggle LED
+
+}
+int main()
+{
+	ADC_Config();
+	configureLED();
+	dma_config();
+	DMA2_Stream0->CR |=(1UL<<0);//Enable DMA
+	ADC1->CR2 |= (1UL<<0); // Start the ADC
+	ADC1->CR2 |= (1UL<<30);// Start the conversion
+	while(1)
+	{
+
+	}
+}
